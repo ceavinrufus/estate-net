9cfb1008e977d043747d9918ed0f75246afa1fbd
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-06-12 18:28:52 +0700

    feat: make contracts

---
+++ contracts/Escrow.sol
@@ -0,0 +1,123 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+interface IERC721 {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _id
+    ) external;
+}
+
+contract Escrow {
+    address public nftAddress;
+    address payable public seller;
+    address public inspector;
+    address public lender;
+
+    modifier onlyBuyer(uint256 _nftID) {
+        require(msg.sender == buyer[_nftID], "Hanya buyer yang dapat memanggil metode ini");
+        _;
+    }
+
+    modifier onlySeller() {
+        require(msg.sender == seller, "Hanya penjual yang dapat memanggil metode ini");
+        _;
+    }
+
+    modifier onlyInspector() {
+        require(msg.sender == inspector, "Hanya inspektur yang dapat memanggil metode ini");
+        _;
+    }
+
+    mapping(uint256 => bool) public isListed;
+    mapping(uint256 => uint256) public purchasePrice;
+    mapping(uint256 => uint256) public escrowAmount;
+    mapping(uint256 => address) public buyer;
+    mapping(uint256 => bool) public inspectionPassed;
+    mapping(uint256 => mapping(address => bool)) public approval;
+
+    constructor(
+        address _nftAddress,
+        address payable _seller,
+        address _inspector,
+        address _lender
+    ) {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        inspector = _inspector;
+        lender = _lender;
+    }
+
+    function list(
+        uint256 _nftID,
+        address _buyer,
+        uint256 _purchasePrice,
+        uint256 _escrowAmount
+    ) public payable onlySeller {
+        // Transfer NFT dari penjual ke kontrak ini
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+        isListed[_nftID] = true;
+        purchasePrice[_nftID] = _purchasePrice;
+        escrowAmount[_nftID] = _escrowAmount;
+        buyer[_nftID] = _buyer;
+    }
+
+    // Menempatkan di Bawah Kontrak (hanya buyer - escrow berbayar)
+    function depositEarnest(uint256 _nftID) public payable onlyBuyer(_nftID) {
+        require(msg.value >= escrowAmount[_nftID]);
+    }
+
+    // Memperbarui Status Inspeksi (hanya inspektur)
+    function updateInspectionStatus(uint256 _nftID, bool _passed)
+        public
+        onlyInspector
+    {
+        inspectionPassed[_nftID] = _passed;
+    }
+
+    // Menyetujui Penjualan
+    function approveSale(uint256 _nftID) public {
+        approval[_nftID][msg.sender] = true;
+    }
+
+    // Menyelesaikan Penjualan
+    // -> Memerlukan status inspeksi (tambahkan item lebih banyak di sini, seperti penilaian)
+    // -> Memerlukan penjualan untuk diotorisasi
+    // -> Memerlukan dana untuk jumlah yang tepat
+    // -> Transfer NFT ke buyer
+    // -> Transfer Dana ke Penjual
+    function finalizeSale(uint256 _nftID) public {
+        require(inspectionPassed[_nftID]);
+        require(approval[_nftID][buyer[_nftID]]);
+        require(approval[_nftID][seller]);
+        require(approval[_nftID][lender]);
+        require(address(this).balance >= purchasePrice[_nftID]);
+
+        isListed[_nftID] = false;
+
+        (bool success, ) = payable(seller).call{value: address(this).balance}(
+            ""
+        );
+        require(success);
+
+        IERC721(nftAddress).transferFrom(address(this), buyer[_nftID], _nftID);
+    }
+
+    // Batalkan Penjualan (menangani earnest deposit)
+    // -> jika status inspeksi tidak disetujui, maka refund, jika tidak, kirim ke penjual
+    function cancelSale(uint256 _nftID) public {
+        if (inspectionPassed[_nftID] == false) {
+            payable(buyer[_nftID]).transfer(address(this).balance);
+        } else {
+            payable(seller).transfer(address(this).balance);
+        }
+    }
+
+    receive() external payable {}
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
